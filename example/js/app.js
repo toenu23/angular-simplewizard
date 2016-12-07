d12ad1512144d168cfcb9987eafef1dc3bf434cb
toenu23 <fubarphukhew@gmail.com>
2016-12-07 14:27:41 +0100

    Update example

--- example/js/app.js
+++ example/js/app.js
@@ -2,38 +2,54 @@ var app = angular.module('App', ['simpleWizard']);
 
 app.controller('MainCtrl', ['$scope', function($scope) {
 
-  $scope.forms = {
-    wizard: {},
-  };
+  // This object will be filled with
+  // the user input gathered form the wizard
+  $scope.wizard = {};
 
+
+  // The template from which our wizard is created
   $scope.wizardTmpl = {
     step1: {
       view: 'views/step1.html',
-      prev: () => false,
-      next: () => 'step2',
+      next: 'step2',
     },
     step2: {
       view: 'views/step2.html',
-      prev: () => 'step1',
-      next: data => data.goToStep3 === '3a' ? 'step3a' : 'step3b',
+      prev: 'step1',
+      next: function(data) {
+        // 'data' contains the contents of the form from all steps
+        // It is the same as '$scope.wizard'
+        // We return the name of the next step as gathered from the form
+        return data.nextStep;
+      },
     },
     step3a: {
       view: 'views/step3a.html',
-      prev: () => 'step2',
-      next: data => data.goToStep3b ? 'step3b' : 'finish',
+      prev: 'step2',
+      next: function(data) {
+        return data.goToStep3b ? 'step3b' : 'finish'
+      },
     },
     step3b: {
       view: 'views/step3b.html',
-      prev: data => data.goToStep3 === '3a' ? 'step3a' : 'step2',
-      next: data => data.something,
+      prev: function(data) {
+        // Depending on where we came from we return to the appropriate
+        // place if the "Back" button is pressed
+        return data.nextStep === 'step3a' ? 'step3a' : 'step2';
+      },
+      next: function(data) {
+        return data.something;
+      },
     },
     deadend: {
       view: 'views/deadend.html',
-      prev: () => 'step3b',
+      prev: 'step3b',
     },
     finish: {
       view: 'views/finish.html',
-      prev: data => ((data.goToStep3 === '3a') && !data.goToStep3b) ? 'step3a' : 'step3b',
+      prev: function(data) {
+        return ((data.nextStep === 'step3a') && !data.goToStep3b) ? 'step3a' : 'step3b';
+      },
     },
   };
 
